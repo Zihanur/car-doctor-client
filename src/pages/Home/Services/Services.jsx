3133107d712b9dfc6e10adc044849dde82670f75
Zihanur Rahman <zihanur15-2188@diu.edu.bd>
2023-05-22 18:12:04 +0600

    search and filter add on services section

--- src/pages/Home/Services/Services.jsx
+++ src/pages/Home/Services/Services.jsx
@@ -1,14 +1,21 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import ServiceCard from "./ServiceCard";
 
 const Services = () => {
   const [services, setServices] = useState([]);
+  const [search, setSearch] = useState("");
+  const [asc, setAsc] = useState(true);
+  const searchRef =  useRef("")
 
   useEffect(() => {
-    fetch("http://localhost:5000/services")
+    fetch(`http://localhost:5000/services?search=${search}&sort=${asc ? "asc" : "desc"}`)
       .then((res) => res.json())
       .then((data) => setServices(data));
-  }, []);
+  }, [asc,search]);
+
+  const handleSearch=()=>{
+    setSearch(searchRef.current.value)
+  }
 
   return (
     <div>
@@ -23,6 +30,37 @@ const Services = () => {
           the majority have suffered alteration in some form, by injected
           humour, or randomised words which donot look even slightly believable.{" "}
         </p>
+        <div className="flex justify-end gap-4">
+          <div className="form-control">
+            <div className="input-group">
+              <input
+              ref={searchRef}
+                type="text"
+                placeholder="Search…"
+                className="input input-bordered"
+              />
+              <button onClick={handleSearch} className="btn btn-square">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
+          <button className="btn btn-primary" onClick={() => setAsc(!asc)}>
+            {asc ? "Price: High to Low" : "Price: Low to High"}
+          </button>
+        </div>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {services.map((service) => (
